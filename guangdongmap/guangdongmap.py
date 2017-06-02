ea5fa9d0af7073d3b4b94f65ba3f0f95fc78a485
690244957 <690244957@qq.com>
2017-06-02 17:33:15 +0800

    guangdongmap
    new

--- guangdongmap/guangdongmap.py
+++ guangdongmap/guangdongmap.py
@@ -1,23 +1,31 @@
- # -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*- 
 from flask import Flask, render_template, request, escape
+#from search5 import search2
 
 app = Flask(__name__)
+
+
+
+@app.route('/pick_city', methods=['POST'])
+def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+    city=request.form['city']
+    title = '以下是您的结果：'
+    #results = search2(city)
+    return render_template('results.html',
+                           the_title=title,
+                           the_city=city,)
+                           #the_results=results,)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     """Display this webapp's HTML form."""
     return render_template('entry.html',
-                           the_title='广东城市地图！')
+                           the_title='广东省城市地图！')
 
-@app.route('/pick_a_city', methods=['POST'])
-def pick_a_city() -> 'html':
-    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    city = request.form['city']	
-    return render_template('results.html',
-                           the_title = '以下是您选取的城市：',
-                           the_city = city,
-                           )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ guangdongmap/lookup_longitude_latitude.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*- 
+with open ('data/new.txt','r',encoding='utf8')as data:
+    gd=data.readlines()
+
+
+xxx=gd[0].strip().split(' ')
+#print(xxx)
+
+yyy ={x.split(':')[0]:x.split(':')[1] for x in xxx}
+#print(yyy)
+
+#print(yyy['纬度'])
+
+line_all=[]
+for line in gd:
+    line_data = line.strip().split(' ')
+    line_dict = {x.split(':')[0]:x.split(':')[1] for x in line_data}
+    # {'经度': '111.48', '城市': '广东封开', '纬度': '23.45;'}
+    line_all.append(line_dict)
+
+line_all_dict={}
+for line in gd:
+    line_data = line.strip().split(' ')
+    line_dict = {x.split(':')[0]:x.split(':')[1] for x in line_data}
+    k=line_dict['城市']
+    v= {'经度':line_dict[ '经度'], '纬度':line_dict[ '纬度']}
+    line_all_dict.update({k:v})
+
+print(line_all_dict['广东从化'])
+print(line_all_dict['广东从化']['经度'])
+
+
+
+def location(city):
+   x=line_all_dict['广东从化']['经度']
+   y=line_all_dict['广东从化']['纬度']
+
+
+   return(x,y)
+
+
+x, y = location('广东从化')
+print(x,y)
+
+import folium
+map_osm = folium.Map(location=[x, y], zoom_start=14)
+folium.Marker([x, y], 
+              popup="广东从化", 
+              icon=folium.Icon(color='red',icon='info-sign')
+             ).add_to(map_osm)
+map_osm.save('map.html')
