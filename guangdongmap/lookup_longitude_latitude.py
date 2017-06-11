b2f2d844072b82ef98c7ede20a04c0628030a2c7
Hejuntian <254586414@qq.com>
2017-06-11 17:32:51 +0800

    Update 期末专案程序可跑

--- guangdongmap/lookup_longitude_latitude.py
+++ guangdongmap/lookup_longitude_latitude.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*- 
+import requests
+
 with open ('data/guangdong.txt','r',encoding='utf8')as data:
     gd=data.readlines()
 
@@ -26,26 +28,37 @@ for line in gd:
     v= {'经度':line_dict[ '经度'], '纬度':line_dict[ '纬度']}
     line_all_dict.update({k:v})
 
-print(line_all_dict['广东从化'])
-print(line_all_dict['广东从化']['经度'])
 
 
+#定义经纬度函数
+def location_x(cities):
+    x=line_all_dict[cities]['经度']  
+    return x
+def location_y(cities):
+    y=line_all_dict[cities]['纬度'] 
+    return y
+def get_location(cities):
+    z=[location_x(cities),location_y(cities)]
+    str1=','.join(z)
+    return str1
 
-def location(city):
-   x=line_all_dict['广东从化']['经度']
-   y=line_all_dict['广东从化']['纬度']
+#抓取地图
+def get_img(cities):
+    path_img = ("maps/{img}.png".format(img=cities))
+    #高德地图（静态地图）api
+    url_api = "http://restapi.amap.com/v3/staticmap"
+    parameters={'location':'',
+                'zoom':10,
+                'size':'1000*600',
+                'key': 'ee83ffb0500bcbbe5929a0d58d012e0e'}
+    parameters['location']=get_location(cities)
+    r = requests.get(url_api, params=parameters)
+    try:
+        data = r.json()
+    except:
+        data = r.content
+    with open (path_img, "wb") as f:
+        f.write(r.content)
+    return path_img
 
 
-   return(x,y)
-
-
-x, y = location('广东从化')
-print(x,y)
-
-import folium
-map_osm = folium.Map(location=[x, y], zoom_start=14)
-folium.Marker([x, y], 
-              popup="广东从化", 
-              icon=folium.Icon(color='red',icon='info-sign')
-             ).add_to(map_osm)
-map_osm.save('map.html')
