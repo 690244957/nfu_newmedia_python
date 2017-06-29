92dbb556beb43427817e27225a184d57e607266f
Jasonkira <924027370@qq.com>
2017-06-29 19:03:06 +0800

    再次增加注释
    再次增加注释

--- chinesemap/lookup_longitude_latitude.py
+++ chinesemap/lookup_longitude_latitude.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-def download():
+def download():  #定义dowloand函数
     #中国各省经纬度资料抓取
     url_api = "http://restapi.amap.com/v3/config/district"
     parameters = {'keywords': '中华人民共和国', 
@@ -27,32 +27,31 @@ def download():
     
 try:
     with open('data/chinesemap.json','r') as fp:
-        PRC_cities = json.load(fp)
+        PRC_cities = json.load(fp)    #尝试打开并读取data/chinesemap.json
 except:
-    PRC_cities = download()
+    PRC_cities = download()      
     with open('data/chinesemap.json','w') as file:
-        json.dump(PRC_cities, file)
+        json.dump(PRC_cities, file)   #如若不行则打开data/chinesemap#
 
 
   
 #抓取地图
-def get_img(a_city, z='10'):
+def get_img(a_city, z='10'):   #定义get_img函数，并且可以输入a_city, z='10'
     path_img = "maps/{img}_{zo}.png".format(img=a_city, zo=z)
     #高德地图（静态地图）api
-    url_api = "http://restapi.amap.com/v3/staticmap"
+    url_api = "http://restapi.amap.com/v3/staticmap"  #api网址
     parameters={'location':'',
                 'zoom':z,
                 'size':'1000*600',
-                'key': 'ee83ffb0500bcbbe5929a0d58d012e0e'}
+                'key': 'ee83ffb0500bcbbe5929a0d58d012e0e'}   #调用api参数
     
     parameters['location']= PRC_cities[a_city]
-    r = requests.get(url_api, params=parameters)
+    r = requests.get(url_api, params=parameters)   #调用api
     try:
-        data = r.json()
+        data = r.json()  #尝试打开json档
     except:
-        data = r.content
+        data = r.content #如果打不开就输出api的原始数据
     with open (path_img, "wb") as f:
-        f.write(r.content)
-    return path_img
-
+        f.write(r.content)  #打开图片并读取图片
+    return path_img   #回到图片
 
